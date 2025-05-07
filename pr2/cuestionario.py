259bef3b4ba27a8858d9b99c5e9fe2d4b1a4fab2
rpd634-ual <rpd634@inlumine.ual.es>
2025-05-07 20:49:21 +0000

    pr2

---
+++ pr2/cuestionario.py
@@ -0,0 +1,59 @@
+# Pregunta 0
+# ¿Estás matriculado en la asignatura? SI
+
+print('SI')
+#########################
+# Pregunta 1  ¿Qué es GitHub?
+# A) Una plataforma para alojar proyectos
+
+print('A')
+#########################
+# Pregunta 2 Cuántos bits hay en 32 KB? ¿Y en 64 MB?  
+# ¿Y en 4 GB? Incluir las respuestas separadas por un espacio
+
+print('262144 536870912 34359738368')
+#########################
+# Pregunta 3 (VERDADERO/FALSO) La información de la unidad de memoria 
+# es persistente: se pierde cuando se apaga el ordenador.
+
+print('FALSO')
+#########################
+# Pregunta 4 ¿Cuántos bits se necesitarían como mínimo para 
+# codificar un conjunto de 200 caracteres?
+
+print('7')
+#########################
+# Pregunta 5  (Completar) Un ----- es la abreviatura de "dígito binario", 
+# un dígito que puede asumir uno de dos valores y es el dato más pequeño de un ordenador.
+# (poner respuesta en mayusculas)
+
+print('BIT')
+#########################
+# Pregunta 6 ¿Cuáles de las siguientes palabras están relacionadas con la memoria? 
+# Imprimir solo las verdaderas en orden
+# A) Unidad aritmético lógica
+# B) Dirección
+# C) Procesadores
+# D) Ninguno de los anteriores
+
+print('B')
+#########################
+# Pregunta 7 Cuando hablamos de **código fuente** nos referimos a
+# A) El programa escrito en lenguaje de alto nivel
+
+print('A')
+#########################
+# Pregunta 8 El procesador central (CPU) está formado por:
+# C) Unidad de control y unidad aritmético lógica
+
+print('C')
+#########################
+# Pregunta 9 La representación en base N. ¿Cuántos símbolos tiene el alfabeto?
+
+print('N')
+#########################
+# Pregunta 10 ¿Cuál de las siguientes opciones representa el sistema numérico binario?
+# B) 0, 1
+
+print('B')
+#########################
